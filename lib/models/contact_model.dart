d4233c028d73716e388f31611399d67cf0e492f5
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-19 19:24:04 +0000

    contact model

---
+++ lib/models/contact_model.dart
@@ -0,0 +1,9 @@
+class ContactModel {
+  String firstName = "";
+  String lastName = "";
+  String email = "";
+  String phoneNumber = "";
+
+  ContactModel(this.firstName, this.lastName, this.email, this.phoneNumber);
+  
+}
